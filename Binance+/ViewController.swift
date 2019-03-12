182c10ca8ef19a9ad5375abc4843b0b897679f9d
Behnam Karimi <behnamkarimi@behnams-macbook-air.local>
2019-03-12 15:07:24 +0330

    Initial Commit

---
+++ Binance+/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Binance+
+//
+//  Created by Behnam Karimi on 12/21/1397 AP.
+//  Copyright © 1397 AP Behnam Karimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
